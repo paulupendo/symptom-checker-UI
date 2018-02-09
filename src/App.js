ce4ee4403a6d3d16cf48509ebad17054ba1d43cc
paulupendo <lovepaul46@outlook.com>
2018-02-10 01:32:22 +0300

    [Feature]Routing config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PatientScreen from './components/PatientScreen';
 import './App.css';
 
-const App = () => {
-  return <div>Here</div>;
-};
+const FourOhFour = () => <h4>404</h4>;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={PatientScreen} />
+      <Route component={FourOhFour} />
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
